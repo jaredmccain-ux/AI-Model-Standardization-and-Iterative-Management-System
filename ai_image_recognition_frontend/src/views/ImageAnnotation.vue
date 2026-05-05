be09759453690f2abbf25cdae491393d10a7b46d
baiyu <1537692189@qq.com>
2026-05-05 23:02:20 +0800

    perf: avoid chunked upload for typical images

--- ai_image_recognition_frontend/src/views/ImageAnnotation.vue
+++ ai_image_recognition_frontend/src/views/ImageAnnotation.vue
@@ -1012,7 +1012,7 @@ const stagingUploadingCount = ref(0);
 const STAGING_UPLOAD_CONCURRENCY = 6;
 const STAGING_CHUNK_SIZE = 8 * 1024 * 1024;
 const STAGING_CHUNK_CONCURRENCY = 4;
-const STAGING_CHUNK_THRESHOLD = 4 * 1024 * 1024;
+const STAGING_CHUNK_THRESHOLD = 16 * 1024 * 1024;
 
 function patchImage(index, patch) {
   const old = uploadedImages.value[index];
